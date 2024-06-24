d2db513da2e9681c828823af890b3515733ab488
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-06-24 21:08:17 +0900

    Add layout and static packing list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,62 @@
+/* eslint-disable react/prop-types */
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 2, description: 'Charger', quantity: 1, packed: false },
+];
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> 🏝️ Far Away 🌴</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip 😍</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity}&nbsp;{item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
